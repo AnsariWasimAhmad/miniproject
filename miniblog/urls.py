2e7c0d5c555c29117cebb05fadf74ee046b4b103
ansari wasim ahmad <wasimahmadansari01@gmail.com>
2025-03-19 15:53:25 +0530

    changes

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+from . import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -42,3 +45,6 @@ urlpatterns = [
 
 
 ]
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
